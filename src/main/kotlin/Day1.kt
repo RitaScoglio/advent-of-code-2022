82610bb664a1e05c7763c766510395373843a29e
Rita Scoglio <54063396+RitaScoglio@users.noreply.github.com>
2022-12-02 07:34:27 +0100

    Day1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,17 +1,18 @@
 import java.io.File
-
-fun readFile(fileName: String): String = File(fileName).readText(Charsets.UTF_8)
-
-fun main() {
-    val input = readFile("Day1.txt")
+class Day1 {
+    fun readFileAsString(fileName: String): String
+            = File(fileName).readText(Charsets.UTF_8)
+    fun main() {
+        val input = readFileAsString("Day1.txt")
             .split("\n\n")
             .map { elf -> elf.split("\n").map { cal -> cal.toInt() } }
             .map { it.sum() }
-    println(firstStar(input))
-    println(secondStar(input))
+        println(firstStar(input))
+        println(secondStar(input))
+    }
+
+    fun firstStar(input: List<Int>): Int = input.maxOf { it }
+
+    fun secondStar(input: List<Int>): Int = input.sortedDescending().take(3).sum()
+
 }
-
-fun firstStar(input: List<Int>): Int = input.maxOf { it }
-
-fun secondStar(input: List<Int>): Int = input.sortedDescending().take(3).sum()
-
